283d437e4fa8fb06559ddb1d2b0e8d5ca79de3f7
Marat <marat.bekmyrza@nu.edu.kz>
2021-05-19 15:05:10 +0600

    added script for command line

--- log_merger.py
+++ log_merger.py
@@ -2,23 +2,12 @@ import json
 
 import time
 import argparse
-import dataclasses
-import random
-import shutil
-
-from datetime import datetime, timedelta
 from pathlib import Path
+from datetime import datetime
 
 
 _LOG_FILENAMES = 'log_a.jsonl', 'log_b.jsonl'
 _OUT_FILENAME = 'log_merged.jsonl'
-
-
-@dataclasses.dataclass
-class LogRecord:
-    log_level: str
-    timestamp: str
-    message: str
 
 
 def _parse_args() -> argparse.Namespace:
@@ -26,91 +15,97 @@ def _parse_args() -> argparse.Namespace:
 
     parser.add_argument(
         'log1_dir',
-        metavar='<LOG1 PATH>',
+        metavar='<LOG1 DIR>',
         type=str,
-        help='path to log1 file',
+        help='path to dir with log1 file',
     )
 
     parser.add_argument(
         'log2_dir',
-        metavar='<LOG2 PATH>',
+        metavar='<LOG2 DIR>',
         type=str,
-        help='path to log2 file',
+        help='path to dir with log2 file',
     )
 
     parser.add_argument(
         '-o',
         type=str,
         default='./',
-        help='path for output file',
+        help='path to dir for output file',
         dest='out_dir'
     )
 
     return parser.parse_args()
 
 
-def _create_out_dir(dir_path: Path) -> None:
-    if not dir_path.exists():
-        dir_path.mkdir(parents=True)
+def _get_logs_filepath(log1_dir: Path, log2_dir: Path) -> (Path, Path):
+    log1_filename, log2_filename = _LOG_FILENAMES
+    log1_path = log1_dir.joinpath(log1_filename)
+    log2_path = log2_dir.joinpath(log2_filename)
 
-#
-# _RECORD_TEMPLATE = LogRecord(
-#     log_level='<LOG_LEVEL>',
-#     timestamp='<TIMESTAMP>',
-#     message='<MESSAGE>',
-# )
-#
-# _MESSAGE_TEMPLATE = json.dumps(dataclasses.asdict(_RECORD_TEMPLATE)).encode('utf-8')
-# _MESSAGE_TEMPLATE += b'\n'
-#
-#
-# def _generate_logfile(log_filepath: Path, start_time: datetime) -> None:
-#     print(f"generating {log_filepath.name}...")
-#     person_name, action = _PERSON_NAME, _ACTION
-#     object, place, when = _OBJECT, _PLACE, _WHEN
-#     log_levels, message_template = _LOG_LEVELS, _MESSAGE_TEMPLATE
-#     rand, td, ln = random.random, timedelta, len
-#
-#     with log_filepath.open('wb') as fh:
-#         current_time = start_time
-#         total_size, max_size = 0, _MAX_LOG_SIZE_BYTES
-#         write = fh.write
-#         while total_size < max_size:
-#             timestamp = f"{current_time.year}-{current_time.month:02}-{current_time.day:02} " \
-#                         f"{current_time.hour}:{current_time.minute:02}:{current_time.second:02}".encode('utf-8')
-#
-#             message = f"{person_name[int(6 * rand())]} " \
-#                       f"{action[int(10 * rand())]} " \
-#                       f"{object[int(8 * rand())]} " \
-#                       f"{place[int(4 * rand())]} " \
-#                       f"{when[int(5 * rand())]}".encode('utf-8')
-#
-#             data = message_template \
-#                 .replace(b'<LOG_LEVEL>', log_levels[int(4 * rand())]) \
-#                 .replace(b'<TIMESTAMP>', timestamp) \
-#                 .replace(b'<MESSAGE>', message)
-#
-#             write(data)
-#             total_size += ln(data)
-#             current_time += td(seconds=int(10 * rand()))
-#
-#
-# def _merge_logs(output_dir: Path) -> None:
-#     start_time = datetime.now()
-#
-#     for log_filename in _LOG_FILENAMES:
-#         log_path = output_dir.joinpath(log_filename)
-#         _generate_logfile(log_path, start_time)
+    if not log1_path.exists():
+        raise FileNotFoundError(f'File "{log1_path}" doesn\'t exist')
+    if not log2_path.exists():
+        raise FileNotFoundError(f'File "{log2_path}" doesn\'t exist')
+
+    return log1_path, log2_path
+
+
+def _get_out_filepath(out_dir: Path) -> Path:
+    if not out_dir.exists():
+        out_dir.mkdir(parents=True)
+    return out_dir.joinpath(_OUT_FILENAME)
+
+
+def _merge_logs(log1_path: Path, log2_path: Path, out_path: Path) -> None:
+    log1_file, log2_file = open(log1_path, 'r'), open(log2_path, 'r')
+    out_file = open(out_path, 'w')
+
+    log1_lines, log2_lines = log1_file.readlines(), log2_file.readlines()
+    counter1, counter2 = 0, 0
+
+    while counter1 < len(log1_lines) and counter2 < len(log2_lines):
+        line1, line2 = log1_lines[counter1], log2_lines[counter2]
+        line1_json = json.loads(line1)
+        line2_json = json.loads(line2)
+
+        line1_time = datetime.strptime(line1_json['timestamp'], '%Y-%m-%d %H:%M:%S')
+        line2_time = datetime.strptime(line2_json['timestamp'], '%Y-%m-%d %H:%M:%S')
+
+        if line1_time < line2_time:
+            data = line1
+            counter1 += 1
+        else:
+            data = line2
+            counter2 += 1
+        out_file.write(data)
+
+    while counter1 < len(log1_lines):  # remaining from the log1
+        data = log1_lines[counter1]
+        out_file.write(data)
+        counter1 += 1
+
+    while counter2 < len(log2_lines):  # remaining from the log2
+        data = log2_lines[counter2]
+        out_file.write(data)
+        counter2 += 1
+
+    log1_file.close()
+    log2_file.close()
+    out_file.close()
 
 
 def main() -> None:
     args = _parse_args()
-
     t0 = time.time()
+
     log1_dir, log2_dir = Path(args.log1_dir), Path(args.log2_dir)
     output_dir = Path(args.out_dir)
-    _create_out_dir(output_dir)
-    # _merge_logs(log1_dir, log2_dir)
+
+    log1_path, log2_path = _get_logs_filepath(log1_dir, log2_dir)
+    out_path = _get_out_filepath(output_dir)
+    _merge_logs(log1_path, log2_path, out_path)
+
     print(f"finished in {time.time() - t0:0f} sec")
 
 
